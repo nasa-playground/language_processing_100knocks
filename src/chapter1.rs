16c4102ce0aef2ea411623826fbe94e1c9b980e1
k-nasa <htilcs1115@gmail.com>
2020-04-18 17:59:33 +0900

    01

--- src/chapter1.rs
+++ src/chapter1.rs
@@ -2,7 +2,20 @@ fn reverse(s: &str) -> String {
     s.chars().rev().collect::<String>()
 }
 
+fn pick_odd(s: &str) -> String {
+    s.chars()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, v)| v)
+        .collect()
+}
+
 #[test]
 fn reverse_test() {
     assert_eq!(reverse("stressed"), "desserts");
+}
+
+#[test]
+fn pick_odd_test() {
+    assert_eq!(pick_odd("パタトクカシーー"), "パトカー");
 }
