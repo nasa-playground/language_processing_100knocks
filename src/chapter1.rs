6c49d81069cdd08212634cbee5a2ff0eeb084712
k-nasa <htilcs1115@gmail.com>
2020-04-18 18:30:29 +0900

    04

--- src/chapter1.rs
+++ src/chapter1.rs
@@ -23,6 +23,23 @@ fn word_len(s: &str) -> Vec<usize> {
     s.split_whitespace().map(|word| word.len()).collect()
 }
 
+fn simbolo_elementare(s: &str) -> HashMap<usize, String> {
+    let whitelist = vec![1, 5, 6, 7, 8, 9, 15, 19];
+
+    s.split_whitespace()
+        .enumerate()
+        .fold(HashMap::new(), |mut hash, (i, v)| {
+            let v = if whitelist.iter().find(|&&x| x == i).is_some() {
+                String::from(&v[0..1])
+            } else {
+                String::from(&v[0..2])
+            };
+
+            hash.insert(i, v);
+            hash
+        })
+}
+
 #[test]
 fn reverse_test() {
     assert_eq!(reverse("stressed"), "desserts");
@@ -31,4 +48,29 @@ fn reverse_test() {
 #[test]
 fn pick_odd_test() {
     assert_eq!(pick_odd("パタトクカシーー"), "パトカー");
+}
+
+#[test]
+fn add_string_test() {
+    assert_eq!(add_string("パトカー", "タクシー"), "パタトクカシーー");
+}
+
+#[test]
+fn word_len_test() {
+    let sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.";
+
+    assert_eq!(
+        word_len(sentence),
+        vec![3, 1, 4, 1, 6, 9, 2, 7, 5, 3, 5, 8, 9, 7, 10]
+    )
+}
+
+#[test]
+fn simbolo_elementare_test() {
+    let s = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.";
+
+    let hash = simbolo_elementare(s);
+
+    // TODO テストケース
+    // めんどくさいからいつか書く(書かない)
 }
